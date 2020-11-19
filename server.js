aa2b6d9d124b97dc672f73ff050467a7d742156d
Zoe Zhang <jin0925aki@gmail.com>
2020-11-19 11:54:18 +1100

    first commit

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const Pusher = require('pusher');
-    const express = require('express');
-    const bodyParser = require('body-parser');
-    const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv');
+const app = express();
 
-    const app = express();
 
-    app.use(cors());
-    app.use(bodyParser.urlencoded({extended: false}));
-    app.use(bodyParser.json());
-    const pusher = new Pusher({
-      appId: 'APP_ID',
-      key: 'APP_KEY',
-      secret: 'APP_SECRET',
-      cluster: 'APP_CLUSTER',
-      encrypted: true
-    });
-    app.set('PORT', process.env.PORT || 5000);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  encrypted: true
+});
+app.set('PORT', process.env.PORT || 5000);
 
-    app.post('/message', (req, res) => {
-      const payload = req.body;
-      pusher.trigger('chat', 'message', payload);
-      res.send(payload)
-    });
+app.post('/message', (req, res) => {
+  const payload = req.body;
+  pusher.trigger('chat', 'message', payload);
+  res.send(payload)
+});
 
-    app.listen(app.get('PORT'), () => 
-      console.log('Listening at ' + app.get('PORT')))
+app.listen(app.get('PORT'), () =>
+  console.log('Listening at ' + app.get('PORT')))
